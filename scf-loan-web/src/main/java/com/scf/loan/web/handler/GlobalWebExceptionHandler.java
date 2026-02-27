d697936660af9d0e96f953916ec430cde68de901
ztcshen <34959898+ztcshen@users.noreply.github.com>
2026-02-27 15:06:31 +0800

    chore: remove core system schema design doc

--- scf-loan-web/src/main/java/com/scf/loan/web/handler/GlobalWebExceptionHandler.java
+++ scf-loan-web/src/main/java/com/scf/loan/web/handler/GlobalWebExceptionHandler.java
@@ -7,12 +7,13 @@ import com.scf.loan.common.base.exception.ValidateException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintViolationException;
 
 /**
  * 全局异常处理
@@ -25,7 +26,7 @@ public class GlobalWebExceptionHandler {
     @ExceptionHandler(ServiceException.class)
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public ResponseDTO<?> handleServiceException(HttpServletRequest request, ServiceException e) {
+    public ResponseDTO<?> handleServiceException(ServiceException e) {
         log.warn("业务异常: {}-{}", e.getMessage(), e.getBusinessMessage(), e);
         return ResponseDTO.fail(e.getResultCode(), e.getBusinessMessage());
     }
@@ -36,11 +37,34 @@ public class GlobalWebExceptionHandler {
     @ExceptionHandler(ValidateException.class)
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public ResponseDTO<?> handleValidateException(HttpServletRequest request, ValidateException e) {
+    public ResponseDTO<?> handleValidateException(ValidateException e) {
         log.warn("参数校验异常: {}", e.getMessage(), e);
         return ResponseDTO.fail(ResultCodeEnum.PARAM_ERROR, ResultCodeEnum.PARAM_ERROR.getMessage() + ":" + e.getMessage());
     }
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public ResponseDTO<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        String message = e.getBindingResult().getFieldErrors().stream()
+                .findFirst()
+                .map(fieldError -> fieldError.getField() + " " + fieldError.getDefaultMessage())
+                .orElse(ResultCodeEnum.PARAM_ERROR.getMessage());
+        log.warn("请求参数校验异常: {}", message, e);
+        return ResponseDTO.fail(ResultCodeEnum.PARAM_ERROR, ResultCodeEnum.PARAM_ERROR.getMessage() + ":" + message);
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    @ResponseStatus(HttpStatus.OK)
+    @ResponseBody
+    public ResponseDTO<?> handleConstraintViolationException(ConstraintViolationException e) {
+        String message = e.getConstraintViolations().stream()
+                .findFirst()
+                .map(v -> v.getPropertyPath() + " " + v.getMessage())
+                .orElse(ResultCodeEnum.PARAM_ERROR.getMessage());
+        log.warn("约束校验异常: {}", message, e);
+        return ResponseDTO.fail(ResultCodeEnum.PARAM_ERROR, ResultCodeEnum.PARAM_ERROR.getMessage() + ":" + message);
+    }
 
     /**
      * 处理所有其他异常
@@ -48,7 +72,7 @@ public class GlobalWebExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseBody
     @ResponseStatus(HttpStatus.OK)
-    public ResponseDTO<?> handleException(HttpServletRequest request, Exception e) {
+    public ResponseDTO<?> handleException(Exception e) {
         log.error("系统异常: {}", e.getMessage(), e);
         return ResponseDTO.fail(ResultCodeEnum.SYSTEM_ERROR);
     }
